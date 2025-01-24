caef4bb0a79de19a7ff50619bdf1d7d37a40d3c8
vinayak977 <vinayak977@gmail.com>
2025-01-24 23:30:19 +0530

    End Of Section 2

---
+++ NZWalks.WebAPI/NZWalks.WebAPI/Data/NZDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.WebAPI.Models.Domain;
+using System.Data;
+
+namespace NZWalks.WebAPI.Data
+{
+    public class NZDbContext:DbContext
+    {
+        public NZDbContext(DbContextOptions<NZDbContext> option):base(option)
+        {
+                
+        }
+        public DbSet<Region>  Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
---
+++ NZWalks.WebAPI/NZWalks.WebAPI/Models/Domain/Region.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalks.WebAPI.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Area { get; set; }
+        public string Lat { get; set; }
+        public string Long { get; set; }
+        public string Population { get; set; }
+
+        //Navigation Property
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
